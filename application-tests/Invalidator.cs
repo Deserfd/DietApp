c29195bdf58d415d5aef8683931ecc901d59e5d4
deserfd <dima67rud@yandex.ru>
2025-05-14 23:45:11 +0300

    Добавил библиотеку тестов

---
+++ application-tests/Invalidator.cs
@@ -0,0 +1,8 @@
+
+namespace DietApp.Tests.Application;
+
+public class Invalidator<TEntity> : IValidator<TEntity> where TEntity : class, IEntity
+{
+    public Task<bool> ValidateAsync(TEntity entity, CancellationToken cancellationToken = default) => 
+        Task.FromResult(false);
+}
